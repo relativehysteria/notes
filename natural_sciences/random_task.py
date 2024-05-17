64393bbcc8a879190ea832d23bc510509f62f027
relativehysteria <rruziro@gmail.com>
2024-05-17 05:14:35 +0200

    random_task.py is in marathon mode by default

--- natural_sciences/random_task.py
+++ natural_sciences/random_task.py
@@ -1,24 +1,68 @@
 #!/usr/bin/env python
 
-import os
+import os.path as path
 from pathlib import Path
 import importlib.machinery
 import importlib.util
-from random import choice
+from random import choice, randint
 
-def walkpath(path):
+
+def walkpath(dir_path):
     files = []
-    for file in path.iterdir():
+    for file in dir_path.iterdir():
         if file.is_dir():
             files += walkpath(file)
         elif str(file).endswith("tasks.py"):
             files.append(file)
     return files
 
-scriptdir = Path(os.path.dirname(os.path.realpath(__file__)))
-tasks_file = choice(walkpath(scriptdir))
 
-loader = importlib.machinery.SourceFileLoader("tasks", str(tasks_file))
-spec = importlib.util.spec_from_loader("tasks", loader)
-module = importlib.util.module_from_spec(spec)
-loader.exec_module(module)
+class Tasks:
+    def __init__(self, scriptdir):
+        self.scriptdir = scriptdir
+        self.tasks     = self.__import_tasks()
+
+
+    def __import_tasks(self):
+        tasks = dict()
+
+        for (idx, taskmod) in enumerate(walkpath(self.scriptdir)):
+            modname = path.basename(path.dirname(taskmod))
+
+            loader = importlib.machinery.SourceFileLoader(modname, str(taskmod))
+            spec = importlib.util.spec_from_loader(modname, loader)
+            module = importlib.util.module_from_spec(spec)
+            loader.exec_module(module)
+
+            module_tasks = [getattr(module, func) for func in dir(module)
+                            if func.startswith("task") and
+                            callable(getattr(module, func))]
+            tasks[modname] = module_tasks
+
+        return tasks
+
+
+    def pop_random(self):
+        while True:
+            if not self.tasks:
+                return None
+
+            tasks = choice(list(self.tasks.keys()))
+
+            if not self.tasks[tasks]:
+                del self.tasks[tasks]
+            else:
+                break
+
+        idx = randint(0, len(self.tasks[tasks]) - 1)
+        return self.tasks[tasks].pop(idx)
+
+
+tasks = Tasks(Path(path.dirname(path.realpath(__file__))))
+
+while True:
+    task = tasks.pop_random()
+    if task is None:
+        break
+    task()
+    print('------------------------------------\n')
