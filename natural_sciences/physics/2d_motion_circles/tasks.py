54e5a8bebf488ee7c054bd5500e444de405b7755
relativehysteria <rruziro@gmail.com>
2024-06-18 20:37:55 +0200

    conceptual questions begun

--- natural_sciences/physics/2d_motion_circles/tasks.py
+++ natural_sciences/physics/2d_motion_circles/tasks.py
@@ -4,6 +4,21 @@ from random import uniform, randint, choice
 from math import sqrt, pi
 
 
+def task_conceptual1():
+    angle = randint(10, 40)
+    distance = randint(10, 300)
+
+    print(f"In vacuum, the range of a ball shot at the {angle} degree angle is {distance}m. The same range can be achieved if the ball is shot at what other angle?")
+    input()
+
+    ## SOLUTION
+    # If you shoot it below 45 at some angle, you will achieve the same by
+    # shooting it above 45 at the same angle, such that both angles always
+    # add up to 90 degrees.
+
+    print(f"{90 - angle} degrees")
+
+
 def task1():
     r = randint(1, 10) * 10
     f = randint(1, 5)
